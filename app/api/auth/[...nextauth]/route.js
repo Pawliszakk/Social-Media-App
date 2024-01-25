fc37868d4fc33af050e5d55c8859df52876b85b0
Pawliszakk <oskar.pawliszak1@o2.pl>
2024-01-25 20:49:55 +0100

    changed next config to avoid auth typescript error

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,4 +1,5 @@
 import NextAuth from 'next-auth';
+
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 import FacebookProvider from 'next-auth/providers/facebook';
